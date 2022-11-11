ee887ba4179c636ab185705c2d4289f6f41286fc
khalid <khalidrahgozar@gmail.com>
2022-11-11 09:25:46 -0800

    commit from eclips

---
+++ src/Kholas/Gitting.java
@@ -0,0 +1,10 @@
+package Kholas;
+
+public class Gitting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am really sorry for whome always gossip");
+	}
+
+}
